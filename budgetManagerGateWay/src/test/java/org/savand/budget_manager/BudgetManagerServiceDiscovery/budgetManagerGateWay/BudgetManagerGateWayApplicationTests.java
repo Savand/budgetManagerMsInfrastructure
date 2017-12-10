2f9f6c489ecf67e5e014a039e05da2230d28a0af
Savand <savka.and@gmail.com>
2017-12-10 21:06:52 +0200

    add ms infrastructure projects

---
+++ budgetManagerGateWay/src/test/java/org/savand/budget_manager/BudgetManagerServiceDiscovery/budgetManagerGateWay/BudgetManagerGateWayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.savand.budget_manager.BudgetManagerServiceDiscovery.budgetManagerGateWay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudgetManagerGateWayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
